56e6867becbfe4da744d0495e7f29c878f1d8fd9
unknown <erik.galbero@gmail.com>
2021-01-12 13:04:43 +0100

    init

--- code/client/src/views/Login.vue
+++ code/client/src/views/Login.vue
@@ -30,7 +30,7 @@
                                         <path d="M10 2a5 5 0 00-5 5v2a2 2 0 00-2 2v5a2 2 0 002 2h10a2 2 0 002-2v-5a2 2 0 00-2-2H7V7a3 3 0 015.905-.75 1 1 0 001.937-.5A5.002 5.002 0 0010 2z" />
                                       </svg>
                                     </span>
-                                  <input type="text" name="password" placeholder="PASSWORD" class="px-3 py-3 block w-full p-2.5 rounded-full bg-white border border-indigo-600 relative focus:outline-none focus:shadow-outline w-full pl-10">
+                                  <input type="password" name="password" placeholder="PASSWORD" class="px-3 py-3 block w-full p-2.5 rounded-full bg-white border border-indigo-600 relative focus:outline-none focus:shadow-outline w-full pl-10">
                                 </div>
 
                                 <div class="flex justify-between p-2 text-sm bg-white">
