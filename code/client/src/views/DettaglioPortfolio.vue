3c3031197d2377eee362338bfe4a55b7e7d1cfc2
Simone <galiottosimone@gmail.com>
2021-01-22 11:53:14 +0100

    fix

--- code/client/src/views/DettaglioPortfolio.vue
+++ code/client/src/views/DettaglioPortfolio.vue
@@ -11,7 +11,7 @@
             <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd" />
           </svg>
 
-          <span class="flex justify-center">Nome Cognome</span><br>
+          <span class="flex justify-center">Nome Cognome hahaha</span><br>
           
           <button type="button" class="border-2 border-blue-600 text-blue-600 rounded-full px-9 py-2 m-2">Visualizza</button><br>
           <button type="button" class="border-2 border-blue-600 text-blue-600 rounded-full px-14 py-2 m-2">Like</button><br>
