aaeb1706550e4231ea338232374b2bcb8ec174ad
MattiaMoretto97 <mattiamoretto05@gmail.com>
2021-01-22 11:09:16 +0100

    add link between models

--- code/server/app/Models/ProjectLiker.php
+++ code/server/app/Models/ProjectLiker.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class ProjectLiker extends Model
 {
     use HasFactory;
+
+    public function User()
+    {
+        return $this->belongsTo(User::class, 'liker_id');
+    }
+    public function Project()
+    {
+        return $this->belongsTo(Project::class, 'project_id');
+    }
 }
