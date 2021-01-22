590f56c06968c2d60ed1aeeb9254e480ca5027bf
Giovanni <capitaniogiovanni498@gmail.com>
2021-01-22 12:13:52 +0100

    ControllerEditor

--- code/server/app/Http/Controllers/ProjectController.php
+++ code/server/app/Http/Controllers/ProjectController.php
@@ -7,13 +7,13 @@ use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    public function store(Request $_REQUEST)
+    public function store(Request $request)
   {
       if($request->get('image'))
        {
           $image = $request->get('image');
           $name = time().'.' . explode('/', explode(':', substr($image, 0, strpos($image, ';')))[1])[1];
-          \Image::make($request->get('image'))->save(public_path('images/').$name);
+          $image::make($request->get('image'))->save(public_path('images/').$name);
         }
 
        $image= new Project();
