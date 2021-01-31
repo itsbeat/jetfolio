bc492c7e3dc8b3865e4747ce3922f89bc70519f9
Alessandro <alessandro@AlessandrosiMac.homenet.telecomitalia.it>
2021-01-31 13:50:24 +0100

    aggiornamento footer

--- code/client/src/views/Homepage.vue
+++ code/client/src/views/Homepage.vue
@@ -366,13 +366,13 @@
       <div class="px-4 md:w-1/4 md:ml-auto mt-6 sm:mt-4 md:mt-6">
         <h5 class="text-xl font-bold mb-2 sm:text-center xl:text-left">Seguici sui social</h5>
         <div class="flex sm:justify-center xl:justify-start">
-          <a href="" class="w-8 h-8 border border-2 border-gray-400 rounded-full text-center py-1 text-black hover:text-white hover:bg-blue-600 hover:border-blue-600">
+          <a href="" class="w-8 h-8 border border-2 border-gray-400 rounded-full text-center py-1 text-white hover:text-white hover:bg-blue-600 hover:border-blue-600">
             <i class="fab fa-instagram"></i>
           </a>
-          <a href="" class="w-8 h-8 border border-2 border-gray-400 rounded-full text-center py-1 ml-2 text-black hover:text-white hover:bg-blue-400 hover:border-blue-400">
+          <a href="" class="w-8 h-8 border border-2 border-gray-400 rounded-full text-center py-1 ml-2 text-white hover:text-white hover:bg-blue-400 hover:border-blue-400">
             <i class="fab fa-twitter"></i>
           </a>
-          <a href="" class="w-8 h-8 border border-2 border-gray-400 rounded-full text-center py-1 ml-2 text-black hover:text-white hover:bg-red-600 hover:border-red-600">
+          <a href="" class="w-8 h-8 border border-2 border-gray-400 rounded-full text-center py-1 ml-2 text-white hover:text-white hover:bg-red-600 hover:border-red-600">
             <i class="fab fa-facebook"></i>
           </a>
         </div>
