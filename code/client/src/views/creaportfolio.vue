2e57d5fef1b212fa9cb962fa03b86ae806069bc1
Alessandro <alessandro@AlessandrosiMac.homenet.telecomitalia.it>
2021-02-02 20:11:37 +0100

    modifiche creaportfolio

--- code/client/src/views/creaportfolio.vue
+++ code/client/src/views/creaportfolio.vue
@@ -1,10 +1,14 @@
 <template>
   <div id="app">
-    <div class="wrapper flex mb-36">
-      <div class="lg:w-2/4 sm:w-full bg-white font-sans text-gray-700 ml-16">
+    <div class="md:wrapper md:flex sm:inline-block mb-36">
+      <div class="lg:w-2/4 sm:w-full bg-white font-sans text-gray-700 md:ml-16 ml-0">
         <div class="container mx-auto p-8 flex"></div>
-            <h1 class="text-indigo-600 text-3xl text-center mb-3">Carica le immagini del tuo portfolio</h1>
-        <div class="container overflow-hidden bg-indigo-100 rounded-md shadow-lg p-6">
+        <h1 class="text-indigo-600 text-3xl text-center mb-3">
+          Carica le immagini del tuo portfolio
+        </h1>
+        <div
+          class="container overflow-hidden bg-indigo-100 rounded-md shadow-lg p-6"
+        >
           <div class="col-md-5 text-center inline">
             <label for="my-file" class="text-indigo-600 block mb-6"
               >Scegli o trascina un'immagine</label
@@ -53,110 +57,120 @@
           </div>
         </div>
       </div>
-      <div class="lg:inline-block w-2/6  px-8 mx-8">
+      <div class="sm:flex sm:flex-col md:w-2/6 sm:w-full  md:px-8 sm:px-4 md:mx-8 sm:mx-4">
         <div class="flex justify-center">
-          
-            <!-- Modal container -->
-            <div
-              class="w-full mx-auto px-4 sm:px-6 lg:px-8"
+          <!-- Modal container -->
+          <div class="w-full mx-auto px-4 sm:px-6 lg:px-8">
+            <!-- Card container -->
+            <form
+              class="px-8 py-6 space-y-6 overflow-hidden bg-white rounded-md shadow-lg mt-6"
             >
-              <!-- Card container -->
-              <form
-                class="px-8 py-6 space-y-6 overflow-hidden bg-white rounded-md shadow-lg mt-6"
-              >
-                <!-- Card heading -->
-                <div class="flex justify-between">
-                  <div class="space-y-2">
-                    <h2 class="font-bold text-gray-900">Nome</h2>
-                    <p class="text-sm font-medium leading-5 text-gray-500">
-                      <input type="text" placeholder="Inserisci il tuo nome" />
-                    </p>
-                  </div>
-                  <div></div>
+              <!-- Card heading -->
+              <div class="flex justify-between">
+                <div class="space-y-2">
+                  <h2 class="font-bold text-gray-900">Nome</h2>
+                  <p class="text-sm font-medium leading-5 text-gray-500">
+                    <input type="text" placeholder="Inserisci il tuo nome" />
+                  </p>
                 </div>
-                <!-- Flow options -->
-                <div class="space-y-6">
-                  <div class="space-y-4">
-                    <label class="block space-y-2">
-                      <span
-                        class="block text-xs font-bold leading-4 tracking-wide uppercase text-gray-600"
-                        >Descrizione del progetto</span
+                <div></div>
+              </div>
+              <!-- Flow options -->
+              <div class="space-y-6">
+                <div class="space-y-4">
+                  <label class="block space-y-2">
+                    <span
+                      class="block text-xs font-bold leading-4 tracking-wide uppercase text-gray-600"
+                      >Descrizione del progetto</span
+                    >
+                    <textarea
+                      rows="5"
+                      class="md:text-left w-full bg-indigo-100 sm:text-center sm-text-sm"
+                    >
+                    </textarea>
+                  </label>
+                  <div class="flex items-center space-x-6">
+                    <div class="space-y-1">
+                      <label
+                        for="flow-name"
+                        class="text-sm font-medium leading-5 text-gray-600"
                       >
-                      <textarea
-                        rows="5"
-                        class="md:text-left w-full bg-indigo-100 sm:text-center sm-text-sm"
-                      >
-                      </textarea>
-                    </label>
-                    <div class="flex items-center space-x-6">
-                      <div class="space-y-1">
-                        <label
-                          for="flow-name"
-                          class="text-sm font-medium leading-5 text-gray-600"
-                        >
-                          Categoria
-                        </label>
-                        <div class="relative flex">
-                          <select
-                            id="currency"
-                            name="currency"
-                            class="focus:ring-indigo-500 focus:border-indigo-500 h-full py-0 pl-2 pr-7 border-transparent bg-transparent text-gray-500 sm:text-sm rounded-md"
-                          >
-                            <option>Programmazione</option>
-                            <option>Grafica</option>
-                            <option>Iot</option>
-                            <option>User Design</option>
-                          </select>
+                        Categoria
+                      </label>
+                      <div class="block">
+                        <div class="mt-2">
+                          <div>
+                            <label class="inline-flex items-center">
+                              <input type="checkbox" class="form-checkbox" />
+                              <span class="ml-2">Programmazione</span>
+                            </label>
+                          </div>
+                          <div>
+                            <label class="inline-flex items-center">
+                              <input type="checkbox" class="form-checkbox" />
+                              <span class="ml-2">Grafica</span>
+                            </label>
+                          </div>
+                          <div>
+                            <label class="inline-flex items-center">
+                              <input type="checkbox" class="form-checkbox" />
+                              <span class="ml-2">Ui Design</span>
+                            </label>
+                            <div>
+                              <label class="inline-flex items-center">
+                                <input type="checkbox" class="form-checkbox" />
+                                <span class="ml-2">Iot</span>
+                              </label>
+                            </div>
+                          </div>
                         </div>
                       </div>
-                      <label class="flex items-center mt-6 space-x-1.5">
-                        <input
-                          type="checkbox"
-                          name="delay-traversable"
-                          class="form-checkbox rounded text-indigo-500"
-                        />
-                        <span
-                          class="flex items-center space-x-1 text-sm leading-5 text-gray-900"
-                        >
-                          <span class="text-sm leading-5 text-gray-900"
-                            >Pubblico</span
-                          >
-                          <svg
-                            class="w-4 h-4 text-gray-400"
-                            fill="none"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                          >
-                            <path
-                              d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                            ></path>
-                          </svg>
-                        </span>
-                      </label>
                     </div>
+
+                    <label class="flex items-center mt-6 space-x-1.5">
+
+                      <!--
+                      <input
+                        type="checkbox"
+                        name="delay-traversable"
+                        class="form-checkbox rounded text-indigo-500"
+                      />
+
+                      <span
+                        class="flex items-center space-x-1 text-sm leading-5 text-gray-900"
+                      >
+                        <span class="text-sm leading-5 text-gray-900"
+                          >Pubblico</span
+                        >
+                        <svg
+                          class="w-4 h-4 text-gray-400"
+                          fill="none"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                        >
+                          <path
+                            d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                          ></path>
+                        </svg>
+                      </span>
+                      -->
+                    </label>
                   </div>
                 </div>
-                <div
-                  class="max-w-md grid grid-flow-row sm:grid-cols-2 gap-3 sm:gap-6"
+              </div>
+              <div class="max-w-md flex justify-center">
+                <button
+                  type="button"
+                  class="px-4 py-3 border border-transparent rounded text-white bg-indigo-600 hover:bg-indigo-500 transition duration-300 ease-in-out"
                 >
-                  <button
-                    type="button"
-                    class="px-4 py-3 border border-gray-200 rounded text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-600 transition duration-300 ease-in-out"
-                  >
-                    Elimina
-                  </button>
-                  <button
-                    type="button"
-                    class="px-4 py-3 border border-transparent rounded text-white bg-indigo-600 hover:bg-indigo-500 transition duration-300 ease-in-out"
-                  >
-                    Pubblica
-                  </button>
-                </div>
-              </form>
-            </div>
+                  Pubblica
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
@@ -166,9 +180,6 @@
 .custom-file-input::-webkit-file-upload-button {
   visibility: hidden;
 }
-.custom-file-input {
-  border: 2px solid black;
-}
 .img-fluid {
   max-width: 30%;
   max-height: 100px;
@@ -176,7 +187,7 @@
 </style>
 <script>
 export default {
-  data: function () {
+  data: function() {
     return {
       preview: null,
       image: null,
@@ -191,7 +202,7 @@ export default {
     };
   },
   methods: {
-    previewMultiImage: function (event) {
+    previewMultiImage: function(event) {
       var input = event.target;
       var count = input.files.length;
       var index = 0;
@@ -208,7 +219,7 @@ export default {
         }
       }
     },
-    reset: function () {
+    reset: function() {
       this.image = null;
       this.preview = null;
       this.image_list = [];
