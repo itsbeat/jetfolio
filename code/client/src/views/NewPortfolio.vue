89f072f5da44eb90c4dfc07c3fcbb914ecca3c1b
Alessandro <alessandro@AlessandrosiMac.homenet.telecomitalia.it>
2021-02-03 20:22:10 +0100

    a

--- code/client/src/views/NewPortfolio.vue
+++ code/client/src/views/NewPortfolio.vue
@@ -11,8 +11,8 @@
               <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd" />
               </svg>  
             </div>
-            <div class="container flex">
-        <div class="row justify-content-center inline-block">
+            <div class="container">
+        <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card card-default">
                     <div class="card-header">File Upload Component</div>
